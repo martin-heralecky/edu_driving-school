dd1a13e9082f80b106676ae8c5d6c78995f50652
Martin Heralecký <heralecky.martin@gmail.com>
2018-12-30 15:50:12 +0100

    add integration/VehicleDAOMap test

---
+++ integration/src/test/java/cz/martinheralecky/edu/driving_school/integration/impl/VehicleDAOMapTest.java
@@ -0,0 +1,34 @@
+package cz.martinheralecky.edu.driving_school.integration.impl;
+
+import cz.martinheralecky.edu.driving_school.integration.VehicleDAO;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class VehicleDAOMapTest {
+    @Test
+    void createGetAllDeleteWorkCorrectly() {
+        var dao = VehicleDAO.getInstance();
+
+        dao.create("1A2 345", "Opel", "Astra", 2000, "black");
+        assertEquals(1, dao.getAll().size());
+        assertEquals("1A2 345", dao.getAll().get(0).getLicensePlate());
+        assertEquals("Opel", dao.getAll().get(0).getMake());
+        assertEquals("Astra", dao.getAll().get(0).getModel());
+        assertEquals(2000, dao.getAll().get(0).getYear());
+        assertEquals("black", dao.getAll().get(0).getColor());
+
+        dao.create("9Z8 7655", "", "", 0, "");
+        assertEquals(2, dao.getAll().size());
+
+        var vehicles = dao.getAll();
+        assertNotEquals(vehicles.get(0).getLicensePlate(), vehicles.get(1).getLicensePlate());
+
+        dao.delete(vehicles.get(0).getID());
+        assertEquals(1, dao.getAll().size());
+
+        dao.delete(vehicles.get(1).getID());
+        assertEquals(0, dao.getAll().size());
+    }
+}
