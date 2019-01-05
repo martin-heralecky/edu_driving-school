55c20bcad1160885dc685642d0b4a13c2ce03ab3
Martin Heralecký <heralecky.martin@gmail.com>
2019-01-05 15:09:21 +0100

    add rich-client/menu bar to main window

--- rich-client/src/main/java/cz/martinheralecky/edu/driving_school/rich_client/MainWindow.java
+++ rich-client/src/main/java/cz/martinheralecky/edu/driving_school/rich_client/MainWindow.java
@@ -1,7 +1,11 @@
 package cz.martinheralecky.edu.driving_school.rich_client;
 
+import cz.martinheralecky.edu.driving_school.rich_client.controller.QuitAction;
 import cz.martinheralecky.edu.driving_school.utils.Messages;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -12,11 +16,21 @@ class MainWindow extends Stage {
     MainWindow() {
         setTitle(Messages.app_name.getCapitalized());
 
-        VBox root = new VBox();
+        var root = new VBox(createMenuBar());
 
-        Scene scene = new Scene(root, 800, 600);
+        var scene = new Scene(root, 800, 600);
         setScene(scene);
 
         show();
+    }
+
+    /**
+     * Creates menu bar.
+     */
+    private Node createMenuBar() {
+        var fileMenu = new Menu(Messages.menu_file.getCapitalized());
+        fileMenu.getItems().add(new ActionMenuItem(Messages.menu_file_quit.getCapitalized(), new QuitAction()));
+
+        return new MenuBar(fileMenu);
     }
 }
