32f97586721183e4fcba3c0b5e8c5fac5a9bd1cc
Martin Heralecký <heralecky.martin@gmail.com>
2019-01-05 13:35:53 +0100

    add rich-client/Action interface

---
+++ rich-client/src/main/java/cz/martinheralecky/edu/driving_school/rich_client/controller/Action.java
@@ -0,0 +1,8 @@
+package cz.martinheralecky.edu.driving_school.rich_client.controller;
+
+/**
+ * Interface declaring the {@link Action#execute()} method.
+ */
+public interface Action {
+    void execute();
+}
