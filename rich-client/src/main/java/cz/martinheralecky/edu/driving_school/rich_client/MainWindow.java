442c8f5aab3eb2e568257e818782e0798c8105a1
Martin Heralecký <heralecky.martin@gmail.com>
2019-01-06 11:51:46 +0100

    add rich-client/vehicles pane to MainWindow

--- rich-client/src/main/java/cz/martinheralecky/edu/driving_school/rich_client/MainWindow.java
+++ rich-client/src/main/java/cz/martinheralecky/edu/driving_school/rich_client/MainWindow.java
@@ -1,5 +1,7 @@
 package cz.martinheralecky.edu.driving_school.rich_client;
 
+import cz.martinheralecky.edu.driving_school.business.Facade;
+import cz.martinheralecky.edu.driving_school.model.Vehicle;
 import cz.martinheralecky.edu.driving_school.rich_client.controller.QuitAction;
 import cz.martinheralecky.edu.driving_school.utils.Messages;
 import javafx.scene.Node;
@@ -26,7 +28,7 @@ class MainWindow extends Stage {
 
         setTitle(Messages.app_name.getCapitalized());
 
-        var root = new VBox(createMenuBar());
+        var root = new VBox(createMenuBar(), createVehiclesPane());
 
         var scene = new Scene(root, 800, 600);
         setScene(scene);
@@ -42,5 +44,21 @@ class MainWindow extends Stage {
         fileMenu.getItems().add(new ActionMenuItem(Messages.menu_file_quit.getCapitalized(), new QuitAction()));
 
         return new MenuBar(fileMenu);
+    }
+
+    /**
+     * Creates a pane for vehicles.
+     */
+    private Node createVehiclesPane() {
+        return new EntityPane.Builder<Vehicle>()
+            .setTitle(Messages.vehicles.getCapitalized())
+            .addColumn(Messages.vehicle_id.getCapitalized(), "ID")
+            .addColumn(Messages.vehicle_licensePlate.getCapitalized(), "licensePlate")
+            .addColumn(Messages.vehicle_make.getCapitalized(), "make")
+            .addColumn(Messages.vehicle_model.getCapitalized(), "model")
+            .addColumn(Messages.vehicle_year.getCapitalized(), "year")
+            .addColumn(Messages.vehicle_color.getCapitalized(), "color")
+            .setRecordsSupplier(facade::getVehicles)
+            .build();
     }
 }
