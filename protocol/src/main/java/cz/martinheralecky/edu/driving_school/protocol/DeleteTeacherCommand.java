d8eb3531e5909fa4bf42706aaf4ec3857a3c0b16
Martin Heralecký <heralecky.martin@gmail.com>
2019-01-21 23:22:39 +0100

    add protocol/teacher commands

---
+++ protocol/src/main/java/cz/martinheralecky/edu/driving_school/protocol/DeleteTeacherCommand.java
@@ -0,0 +1,27 @@
+package cz.martinheralecky.edu.driving_school.protocol;
+
+import cz.martinheralecky.edu.driving_school.model.Teacher;
+
+/**
+ * A {@link Command} that deletes a teacher, when executed.
+ */
+public class DeleteTeacherCommand extends FacadeCommand<Void> {
+    /**
+     * ID of the entity that is to be deleted.
+     */
+    private final Teacher.ID teacherID;
+
+    /**
+     * @param id ID of the entity to delete.
+     */
+    public DeleteTeacherCommand(Teacher.ID id) {
+        this.teacherID = id;
+    }
+
+    @Override
+    public Void execute() {
+        facade.deleteTeacher(teacherID);
+
+        return null;
+    }
+}
