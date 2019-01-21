015c8adae560dbd98f73ebef2708672782a30d6a
Martin Heralecký <heralecky.martin@gmail.com>
2019-01-21 23:20:07 +0100

    add protocol/vehicle commands

---
+++ protocol/src/main/java/cz/martinheralecky/edu/driving_school/protocol/AddVehicleCommand.java
@@ -0,0 +1,32 @@
+package cz.martinheralecky.edu.driving_school.protocol;
+
+import cz.martinheralecky.edu.driving_school.model.Vehicle;
+
+/**
+ * A {@link Command} that adds a vehicle, when executed.
+ */
+public class AddVehicleCommand extends FacadeCommand<Void> {
+    /**
+     * The entity that is to be added.
+     */
+    private final Vehicle vehicle;
+
+    /**
+     * @param vehicle The entity to add.
+     */
+    public AddVehicleCommand(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    @Override
+    public Void execute() {
+        facade.addVehicle(
+            vehicle.getLicensePlate(),
+            vehicle.getMake(),
+            vehicle.getModel(),
+            vehicle.getYear(),
+            vehicle.getColor());
+
+        return null;
+    }
+}
---
+++ protocol/src/main/java/cz/martinheralecky/edu/driving_school/protocol/DeleteVehicleCommand.java
@@ -0,0 +1,27 @@
+package cz.martinheralecky.edu.driving_school.protocol;
+
+import cz.martinheralecky.edu.driving_school.model.Vehicle;
+
+/**
+ * A {@link Command} that deletes a vehicle, when executed.
+ */
+public class DeleteVehicleCommand extends FacadeCommand<Void> {
+    /**
+     * ID of the entity that is to be deleted.
+     */
+    private final Vehicle.ID vehicleID;
+
+    /**
+     * @param id ID of the entity to delete.
+     */
+    public DeleteVehicleCommand(Vehicle.ID id) {
+        this.vehicleID = id;
+    }
+
+    @Override
+    public Void execute() {
+        facade.deleteVehicle(vehicleID);
+
+        return null;
+    }
+}
