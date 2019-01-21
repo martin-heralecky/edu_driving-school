dbe982992127f30cbbd1feeca2a3000f44f03d0e
Martin Heralecký <heralecky.martin@gmail.com>
2019-01-21 23:24:17 +0100

    add protocol/student commands

---
+++ protocol/src/main/java/cz/martinheralecky/edu/driving_school/protocol/DeleteStudentCommand.java
@@ -0,0 +1,27 @@
+package cz.martinheralecky.edu.driving_school.protocol;
+
+import cz.martinheralecky.edu.driving_school.model.Student;
+
+/**
+ * A {@link Command} that deletes a student, when executed.
+ */
+public class DeleteStudentCommand extends FacadeCommand<Void> {
+    /**
+     * ID of the entity that is to be deleted.
+     */
+    private final Student.ID studentID;
+
+    /**
+     * @param id ID of the entity to delete.
+     */
+    public DeleteStudentCommand(Student.ID id) {
+        this.studentID = id;
+    }
+
+    @Override
+    public Void execute() {
+        facade.deleteStudent(studentID);
+
+        return null;
+    }
+}
