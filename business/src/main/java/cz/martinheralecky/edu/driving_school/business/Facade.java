3010afe36f1967eda0c2e597a673ec71b194ef97
Martin Heralecký <heralecky.martin@gmail.com>
2019-01-25 10:46:47 +0100

    add business/throws clauses to Facade methods

--- business/src/main/java/cz/martinheralecky/edu/driving_school/business/Facade.java
+++ business/src/main/java/cz/martinheralecky/edu/driving_school/business/Facade.java
@@ -8,52 +8,61 @@ import java.time.LocalDate;
 import java.util.List;
 
 /**
- * Interface defining basic operations with vehicles, teachers and students. Operations that change state notify the 
+ * Interface defining basic operations with vehicles, teachers and students. Operations that change state notify the
  * registered observers.
  */
 public interface Facade extends Observable {
     /**
      * Adds a new vehicle.
      */
-    void addVehicle(String licensePlate, String make, String model, int year, String color);
+    void addVehicle(String licensePlate, String make, String model, int year, String color)
+        throws Exception;
 
     /**
      * Retrieves all vehicles.
      */
-    List<Vehicle> getVehicles();
+    List<Vehicle> getVehicles()
+        throws Exception;
 
     /**
      * Deletes a vehicle identified by its ID.
      */
-    void deleteVehicle(Vehicle.ID id);
+    void deleteVehicle(Vehicle.ID id)
+        throws Exception;
 
     /**
      * Adds a new teacher.
      */
-    void addTeacher(String firstName, String surname, String email, String phoneNumber, LocalDate birthDate);
+    void addTeacher(String firstName, String surname, String email, String phoneNumber, LocalDate birthDate)
+        throws Exception;
 
     /**
      * Retrieves all teachers.
      */
-    List<Teacher> getTeachers();
+    List<Teacher> getTeachers()
+        throws Exception;
 
     /**
      * Deletes a teacher identified by ID.
      */
-    void deleteTeacher(Teacher.ID id);
+    void deleteTeacher(Teacher.ID id)
+        throws Exception;
 
     /**
      * Adds a new student.
      */
-    void addStudent(String firstName, String surname, String email, String phoneNumber, LocalDate birthDate);
+    void addStudent(String firstName, String surname, String email, String phoneNumber, LocalDate birthDate)
+        throws Exception;
 
     /**
      * Retrieves all students.
      */
-    List<Student> getStudents();
+    List<Student> getStudents()
+        throws Exception;
 
     /**
      * Deletes a student identified by ID.
      */
-    void deleteStudent(Student.ID id);
+    void deleteStudent(Student.ID id)
+        throws Exception;
 }
--- business/src/main/java/cz/martinheralecky/edu/driving_school/business/impl/FacadeDefault.java
+++ business/src/main/java/cz/martinheralecky/edu/driving_school/business/impl/FacadeDefault.java
@@ -46,52 +46,61 @@ public class FacadeDefault implements Facade {
     }
 
     @Override
-    public void addVehicle(String licensePlate, String make, String model, int year, String color) {
+    public void addVehicle(String licensePlate, String make, String model, int year, String color)
+        throws Exception {
         vehicleDAO.create(licensePlate, make, model, year, color);
         observers.forEach(Observer::update);
     }
 
     @Override
-    public List<Vehicle> getVehicles() {
+    public List<Vehicle> getVehicles()
+        throws Exception {
         return vehicleDAO.getAll();
     }
 
     @Override
-    public void deleteVehicle(Vehicle.ID id) {
+    public void deleteVehicle(Vehicle.ID id)
+        throws Exception {
         vehicleDAO.delete(id);
         observers.forEach(Observer::update);
     }
 
     @Override
-    public void addTeacher(String firstName, String surname, String email, String phoneNumber, LocalDate birthDate) {
+    public void addTeacher(String firstName, String surname, String email, String phoneNumber, LocalDate birthDate)
+        throws Exception {
         teacherDAO.create(firstName, surname, email, phoneNumber, birthDate);
         observers.forEach(Observer::update);
     }
 
     @Override
-    public List<Teacher> getTeachers() {
+    public List<Teacher> getTeachers()
+        throws Exception {
         return teacherDAO.getAll();
     }
 
     @Override
-    public void deleteTeacher(Teacher.ID id) {
+    public void deleteTeacher(Teacher.ID id)
+        throws Exception {
         teacherDAO.delete(id);
         observers.forEach(Observer::update);
     }
 
     @Override
-    public void addStudent(String firstName, String surname, String email, String phoneNumber, LocalDate birthDate) {
+    public void addStudent(String firstName, String surname, String email, String phoneNumber, LocalDate birthDate)
+        throws Exception {
         studentDAO.create(firstName, surname, email, phoneNumber, birthDate);
         observers.forEach(Observer::update);
     }
 
     @Override
-    public List<Student> getStudents() {
+    public List<Student> getStudents()
+        throws Exception {
         return studentDAO.getAll();
     }
 
     @Override
-    public void deleteStudent(Student.ID id) {
+    public void deleteStudent(Student.ID id)
+        throws Exception {
         studentDAO.delete(id);
         observers.forEach(Observer::update);
     }
