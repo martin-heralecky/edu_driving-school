93eb39bfacf534b52b334a4f4d2c57dc9db302bd
Martin Heralecký <heralecky.martin@gmail.com>
2019-01-27 20:59:37 +0100

    add utils/Config class

---
+++ utils/src/main/java/cz/martinheralecky/edu/driving_school/utils/Config.java
@@ -0,0 +1,14 @@
+package cz.martinheralecky.edu.driving_school.utils;
+
+/**
+ * Construct holding the application's configuration.
+ */
+public class Config {
+    public static final int SERVER_PORT = 20666;
+
+    /**
+     * Uninstantiatable.
+     */
+    private Config() {
+    }
+}
